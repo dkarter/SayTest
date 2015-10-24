62a52dfa53a7bf4b7e2b382050f70fb1a8292e14
Dorian Karter <dkarter@gmail.com>
2015-10-24 13:27:38 -0500

    Echo name of voice before playing the sample

--- say.sh
+++ say.sh
@@ -1,64 +1,128 @@
+echo 'Agnes'
 say -v 'Agnes' "Isn't it nice to have a computer that will talk to you?"
+echo 'Albert'
 say -v 'Albert' "I have a frog in my throat. No, I mean a real frog!"
+echo 'Alex'
 say -v 'Alex' "Most people recognize me by my voice."
+echo 'Alice'
 say -v 'Alice' "Salve, mi chiamo Alice e sono una voce italiana."
+echo 'Alva'
 say -v 'Alva' "Hej, jag heter Alva. Jag är en svensk röst."
+echo 'Amelie'
 say -v 'Amelie' "Bonjour, je m’appelle Amelie. Je suis une voix canadienne."
+echo 'Anna'
 say -v 'Anna' "Hallo, ich heiße Anna und ich bin eine deutsche Stimme."
+echo 'Bad News'
 say -v 'Bad News' "The light you see at the end of the tunnel is the headlamp of a fast approaching train."
+echo 'Bahh'
 say -v 'Bahh' "Do not pull the wool over my eyes."
+echo 'Bells'
 say -v 'Bells' "Time flies when you are having fun."
+echo 'Boing'
 say -v 'Boing' "Spring has sprung, fall has fell, winter's here and it's colder than usual."
+echo 'Bruce'
 say -v 'Bruce' "I sure like being inside this fancy computer"
+echo 'Bubbles'
 say -v 'Bubbles' "Pull the plug! I'm drowning!"
+echo 'Carmit'
 say -v 'Carmit' "שלום. קוראים לי כרמית, ואני קול בשפה העברית."
+echo 'Cellos'
 say -v 'Cellos' "Doo da doo da dum dee dee doodly doo dum dum dum doo da doo da doo da doo da doo da doo da doo"
+echo 'Damayanti'
 say -v 'Damayanti' "Halo, nama saya Damayanti. Saya berbahasa Indonesia."
+echo 'Daniel'
 say -v 'Daniel' "Hello, my name is Daniel. I am a British-English voice."
+echo 'Deranged'
 say -v 'Deranged' "I need to go on a really long vacation."
+echo 'Diego'
 say -v 'Diego' "Hola, me llamo Diego y soy una voz española."
+echo 'Ellen'
 say -v 'Ellen' "Hallo, mijn naam is Ellen. Ik ben een Belgische stem."
+echo 'Fiona'
 say -v 'Fiona' "Hello, my name is Fiona. I am a Scottish-English voice."
+echo 'Fred'
 say -v 'Fred' "I sure like being inside this fancy computer"
+echo 'Good News'
 say -v 'Good News' "Congratulations you just won the sweepstakes and you don't have to pay income tax again."
+echo 'Hysterical'
 say -v 'Hysterical' "Please stop tickling me!"
+echo 'Ioana'
 say -v 'Ioana' "Bună, mă cheamă Ioana . Sunt o voce românească."
+echo 'Joana'
 say -v 'Joana' "Olá, chamo-me Joana e dou voz ao português falado em Portugal."
+echo 'Junior'
 say -v 'Junior' "My favorite food is pizza."
+echo 'Kanya'
 say -v 'Kanya' "สวัสดีค่ะ ดิฉันชื่อKanya"
+echo 'Karen'
 say -v 'Karen' "Hello, my name is Karen. I am an Australian-English voice."
+echo 'Kathy'
 say -v 'Kathy' "Isn't it nice to have a computer that will talk to you?"
+echo 'Kyoko'
 say -v 'Kyoko' "こんにちは、私の名前はKyokoです。日本語の音声をお届けします。"
+echo 'Laura'
 say -v 'Laura' "Ahoj. Volám sa Laura . Som hlas v slovenskom jazyku."
+echo 'Lekha'
 say -v 'Lekha' "नमस्कार, मेरा नाम लेखा है.Lekha मै हिंदी मे बोलने वाली आवाज़ हूँ."
+echo 'Luciana'
 say -v 'Luciana' "Olá, o meu nome é Luciana e a minha voz corresponde ao português que é falado no Brasil"
+echo 'Maged'
 say -v 'Maged' "مرحبًا اسمي Maged. أنا عربي من السعودية."
+echo 'Mariska'
 say -v 'Mariska' "Üdvözlöm! Mariska vagyok. Én vagyok a magyar hang."
+echo 'Mei-Jia'
 say -v 'Mei-Jia' "您好，我叫美佳。我說國語。"
+echo 'Melina'
 say -v 'Melina' "Γεια σας, ονομάζομαι Melina. Είμαι μια ελληνική φωνή."
+echo 'Milena'
 say -v 'Milena' "Здравствуйте, меня зовут Milena. Я – русский голос системы."
+echo 'Moira'
 say -v 'Moira' "Hello, my name is Moira. I am an Irish-English voice."
+echo 'Monica'
 say -v 'Monica' "Hola, me llamo Monica y soy una voz española."
+echo 'Nora'
 say -v 'Nora' "Hei, jeg heter Nora. Jeg er en norsk stemme."
+echo 'Paulina'
 say -v 'Paulina' "Hola, me llamo Paulina y soy una voz mexicana."
+echo 'Pipe Organ'
 say -v 'Pipe Organ' "We must rejoice in this morbid voice."
+echo 'Princess'
 say -v 'Princess' "When I grow up I'm going to be a scientist."
+echo 'Ralph'
 say -v 'Ralph' "The sum of the squares of the legs of a right triangle is equal to the square of the hypotenuse."
+echo 'Samantha'
 say -v 'Samantha' "Hello, my name is Samantha. I am an American-English voice."
+echo 'Sara'
 say -v 'Sara' "Hej, jeg hedder Sara. Jeg er en dansk stemme."
+echo 'Satu'
 say -v 'Satu' "Hei, minun nimeni on Satu. Olen suomalainen ääni."
+echo 'Sin-ji'
 say -v 'Sin-ji' "您好，我叫 Sin-ji。我講廣東話。"
+echo 'Tessa'
 say -v 'Tessa' "Hello, my name is Tessa. I am a South African-English voice."
+echo 'Thomas'
 say -v 'Thomas' "Bonjour, je m’appelle Thomas. Je suis une voix française."
+echo 'Ting-Ting'
 say -v 'Ting-Ting' "您好，我叫Ting-Ting。我讲中文普通话。"
+echo 'Trinoids'
 say -v 'Trinoids' "We cannot communicate with these carbon units."
+echo 'Veena'
 say -v 'Veena' "Hello, my name is Veena. I am an Indian-English voice."
+echo 'Vicki'
 say -v 'Vicki' "Isn't it nice to have a computer that will talk to you?"
+echo 'Victoria'
 say -v 'Victoria' "Isn't it nice to have a computer that will talk to you?"
+echo 'Whisper'
 say -v 'Whisper' "Pssssst, hey you, Yeah you, Who do ya think I'm talking to, the mouse?"
+echo 'Xander'
 say -v 'Xander' "Hallo, mijn naam is Xander. Ik ben een Nederlandse stem."
+echo 'Yelda'
 say -v 'Yelda' "Merhaba, benim adım Yelda. Ben Türkçe bir sesim."
+echo 'Yuna'
 say -v 'Yuna' "안녕하세요. 제 이름은 Yuna입니다. 저는 한국어 음성입니다."
+echo 'Zarvox'
 say -v 'Zarvox' "That looks like a peaceful planet."
+echo 'Zosia'
 say -v 'Zosia' "Witaj. Mam na imię Zosia, jestem głosem kobiecym dla języka polskiego."
+echo 'Zuzana'
 say -v 'Zuzana' "Dobrý den, jmenuji se Zuzana. Jsem český hlas."
